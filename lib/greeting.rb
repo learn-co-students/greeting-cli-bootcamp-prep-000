9d0703020c31cbfdf50effd3b0b722b59728b761
Megan McLean <mmclean@protonmail.com>
2017-01-13 12:53:57 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,9 @@
-# code the #greeting method here!
+# Learn.co Bootcamp Prep - Ruby Fundamentals - 12: Greeting CLI Lab
+
+# Define a method, 'greeting', that accepts one parameter(s):
+# 'name'.
+# Print hello message.
+
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+end
