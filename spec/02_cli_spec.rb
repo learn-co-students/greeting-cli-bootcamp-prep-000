d61a67a865f867db79211663ac24fcfddd7f0883
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-12-11 05:44:43 +0000

    Done.

--- spec/02_cli_spec.rb
+++ spec/02_cli_spec.rb
@@ -5,22 +5,21 @@ describe './bin/greet executing a CLI Application' do
     allow($stdout).to receive(:puts)
     allow(self).to receive(:gets).and_return("Don")
 
-    expect($stdout).to receive(:puts).with(rspec ./spec/02_cli_spec.rb:31 # ./bin/greet executing a CLI Application calls on the #greeting method with an argument of the user's name and returns the new greeting, interpolatin
-g the user's name), "Make sure bin/greet has code that can output 'Hi! I'm HAL, what's your name?' exactly."
+    expect($stdout).to receive(:puts).with("Hi! I'm HAL, what's your name?"), "Make sure bin/greet has code that can output 'Hi! I'm HAL, what's your name?' exactly."
 
     run_file("./bin/greet")
   end
 
-  it 'uses #gets.strip to capture the user input and set it equal to a variable called name' do
+  it 'uses #gets.strip to capture the user input and set it equal to a variable called name' do 
     allow($stdout).to receive(:puts)
-
+    
     expect(self).to receive(:gets).and_return("Don")
     name = get_variable_from_file("./bin/greet", "name")
 
     expect(name).to eq("Don")
   end
 
-  it "calls on the #greeting method with an argument of the user's name" do
+  it "calls on the #greeting method with an argument of the user's name" do 
     allow($stdout).to receive(:puts)
     allow(self).to receive(:gets).and_return("Don")
 
@@ -29,7 +28,7 @@ g the user's name), "Make sure bin/greet has code that can output 'Hi! I'm HAL, 
     run_file("./bin/greet")
   end
 
-  it "calls on the #greeting method with an argument of the user's name and returns the new greeting, interpolating the user's name" do
+  it "calls on the #greeting method with an argument of the user's name and returns the new greeting, interpolating the user's name" do 
     allow($stdout).to receive(:puts)
     allow(self).to receive(:gets).and_return("Don")
 
