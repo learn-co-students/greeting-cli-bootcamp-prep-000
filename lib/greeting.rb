497c139bb46bd27cc3608afd8677c5e71683b835
Allison Tzadok <allisontzadok@gmail.com>
2017-12-17 20:44:04 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,7 @@
-# code the #greeting method here!
+#a method that takes an argument of a person's name
+#method uses string interpolation
+#prints "hello #{name}. It's nice to meet you.
+
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+end
