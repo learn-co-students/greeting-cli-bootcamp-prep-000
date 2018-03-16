2a34480d689b4172f13dbd05c82153860dee246e
Shirley Collins <shirley.ednave@gmail.com>
2018-03-16 22:16:21 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,12 @@
 # code the #greeting method here!
+<<<<<<< HEAD
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
 end
+=======
+def greeting(nane)
+  puts "Hello #{name}. It's nice to meet you!"
+end
+
+greeting("jack")
+>>>>>>> f87e89620585682129d8a12502ba2f57b810cd83
