d333f8442c5b290ea7ee93d3964e6474698c8a5b
Annie Eby <eby.annie@gmail.com>
2018-09-13 21:13:59 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,9 @@
-# code the #greeting method here!
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+end
+
+# def halhi
+#   puts "Hi! I'm HAL, what's your name?"
+#   name = gets.strip
+#   greeting(name)
+# end
