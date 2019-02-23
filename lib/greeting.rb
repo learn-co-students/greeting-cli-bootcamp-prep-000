580a887764faba150eb5a3044bbefbca00875478
David zhang <d.ydzdavid@yahoo.com>
2019-02-23 14:52:35 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,6 @@
 # code the #greeting method here!
+require 'pry'
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+  binding.pry
+end       
--- spec/01_greeting_spec.rb
+++ spec/01_greeting_spec.rb
@@ -1,4 +1,4 @@
-require_relative "../lib/greeting.rb"
+  require_relative "../lib/greeting.rb"
 
 describe './lib/greeting.rb' do
   it 'defines a greeting method' do
