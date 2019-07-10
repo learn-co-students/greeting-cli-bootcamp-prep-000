410334be051dd669b7747988352dbb360376a759
corbinarnett <corbin.c.arnett@gmail.com>
2019-07-09 21:56:10 -0700

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,7 @@
 # code the #greeting method here!
+#takes in an argument of a persons name
 def greeting(name)
-    puts "Hello #{name.chomp.strip}. It's nice to meet you."
+    #greets the person whose name was passed in as an argument
+    #greets the person with additional whitespace removed, using .strip
+    puts "Hello #{name.strip}. It's nice to meet you."
 end
