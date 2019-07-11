0eb004a58cbb1c2f630edaa16bd5bcb3a97cdfd4
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-11 13:42:22 +0000

    Done.

--- spec/02_cli_spec.rb
+++ spec/02_cli_spec.rb
@@ -1,5 +1,6 @@
 require_relative "../lib/greeting.rb"
 
+
 describe './bin/greet executing a CLI Application' do
   it 'outputs a welcome message and asks the user for input' do
     allow($stdout).to receive(:puts)
